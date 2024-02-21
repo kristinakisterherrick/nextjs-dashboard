62ff2273194074ae58a2ffb10968a1cb9444cdc5
Kristina Herrick <kristinaherrick@gmail.com>
2024-02-20 17:26:38 -0800

    typo

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -23,7 +23,7 @@ export default async function Page({
             <div className='flex w-full items-center justify-between'>
                 <h1 className={'${lusitana.className} text-2xl'}>Invoices</h1>
             </div>
-            <div className="mt-4 flex items-center justify-between gap2 md:mt-8"ß>
+            <div className="mt-4 flex items-center justify-between gap2 md:mt-8">
                 <Search placeholder="Search invoices..." />
                 <CreateInvoice />
             </div>
