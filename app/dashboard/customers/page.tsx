eaf2453f3fbb1a39c96e0a4438ec7d17a985e5c2
Kristina Herrick <kristinaherrick@gmail.com>
2024-02-20 18:33:27 -0800

    Faker and Chartjs

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,42 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Scatter } from 'react-chartjs-2';
+import faker from '@faker-js/faker';
+
+ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
+
+export const options = {
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+};
+
+export const data = {
+  datasets: [
+    {
+      label: 'A dataset',
+      data: Array.from({ length: 100 }, () => ({
+        x: faker.faker.number.int({ min: -100, max: 100 }),
+        y: faker.faker.number.int({ min: -100, max: 100 }),
+      })),
+      backgroundColor: 'rgba(255, 99, 132, 1)',
+    },
+  ],
+};
+
+// export function App() {
+//   return <Scatter options={options} data={data} />;
+// }
+
 export default function Page() {
-    return <p>Customers Page</p>;
+    return <Scatter options={options} data={data} />;
 }
